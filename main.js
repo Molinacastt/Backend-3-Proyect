690e3ce7fcee3da2565c718c67bda197c205a21a
Alejandro Molina <molina_castt@hotmail.com>
2024-02-03 15:16:41 -0500

    Update

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import { Product } from "./Product.js";
 import { ProductManager } from "./ProductManager.js";
 
-const producto1 = new Product("Arroz", "Rico", 1, 130, "A123")
-const producto2 = new Product("Jabon", "Limpia mucho", 2, 220, "B435")
-const producto3 = new Product("Arena", "Para gatos", 1, 450, "C674")
+const arroz = new Product("Arroz", "Rico", 1, 130, "A123")
+const jabon = new Product("Jabon", "Limpia mucho", 2, 220, "B435")
+const arena = new Product("Arena", "Para gatos", 1, 450, "C674")
 
-const producto1ver2 = new Product("Arroz", "Rico", 14555, 15430, "A123")
+const arroz2 = new Product("Arroz", "Rico", 14555, 15430, "A123")
 
 const productManager = new ProductManager('./products.json')
 
-// console.log(productManager.addProduct(producto2))
+// console.log(productManager.addProduct(jabon))
 // productManager.getProducts()
 // productManager.getProductsById('c51429a5006e')
 // productManager.updateProducto("df5ccd61afce", producto1ver2)
-productManager.deleteProducto("6a76c496f480")
+// productManager.deleteProducto("d989e9242c71")
