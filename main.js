e1c68c6faee3da7b1e271fb1d731cef58263f962
Alejandro Molina <molina_castt@hotmail.com>
2024-02-03 15:22:40 -0500

    First commit

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import { Product } from "./Product.js";
 import { ProductManager } from "./ProductManager.js";
 
-const arroz = new Product("Arroz", "Rico", 1, 130, "A123")
-const jabon = new Product("Jabon", "Limpia mucho", 2, 220, "B435")
-const arena = new Product("Arena", "Para gatos", 1, 450, "C674")
+const arroz = new Product("Arroz", "Rico", 1, 130, "A123");
+const jabon = new Product("Jabon", "Limpia mucho", 2, 220, "B435");
+const arena = new Product("Arena", "Para gatos", 1, 450, "C674");
 
-const arroz2 = new Product("Arroz", "Rico", 14555, 15430, "A123")
+const arroz2 = new Product("Arroz", "Rico", 14555, 15430, "A123");
 
-const productManager = new ProductManager('./products.json')
+const productManager = new ProductManager("./products.json");
 
 // console.log(productManager.addProduct(jabon))
 // productManager.getProducts()
